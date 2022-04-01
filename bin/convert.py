e72b5f36c7b1d79d2bf21e7c4a93b13d015aa1b2
Hirokazu Chiba <hchiba1@gmail.com>
2022-04-01 16:56:04 +0900

    Refactor bin/convert.py

--- bin/convert.py
+++ bin/convert.py
@@ -13,14 +13,20 @@ parser.add_argument('--separate', action='store_true', help='sparql-support.dbcs
 args = parser.parse_args()
 
 fp = open(args.markdown, 'r')
-header = []
 for line in fp:
     line = line.rstrip()
     if re.search('https://togodx.dbcls.jp/human/\?togoKey=', line):
         if args.g3:
             line = re.sub('togodx.dbcls.jp', 'togodx-attribute-g3.dbcls.jp', line)
-        elif args.kohan:
-            line = re.sub('https://togodx.dbcls.jp/human', 'http://ep.dbcls.jp/togodx-server-kohan-pg/build', line)
+        else:
+            if args.kohan:
+                line = re.sub('https://togodx.dbcls.jp/human', 'http://ep.dbcls.jp/togodx-server-kohan-pg/build', line)
+            elif args.separate:
+                line = re.sub('togodx.dbcls.jp/human', 'sparql-support.dbcls.jp/dxs_separate_table/build', line)
+            elif args.dxs:
+                line = re.sub('togodx.dbcls.jp/human', 'sparql-support.dbcls.jp/dxs/build', line)
+            elif args.dxsk:
+                line = re.sub('togodx.dbcls.jp/human', 'sparql-support.dbcls.jp/dxsk/build', line)
             line = re.sub('\?togoKey=', '?dataset=', line)
             line = re.sub('&keys=', '&annotations=', line)
             line = re.sub('&values=', '&filters=', line)
@@ -28,33 +34,4 @@ for line in fp:
             line = re.sub('%2C%22ids%22%3A', '%2C%22nodes%22%3A', line)
             line = re.sub('%7B%22categoryId%22', '%7B%22node%22', line)
             line = re.sub('%22ancestors%22', '%22path%22', line)
-        elif args.separate:
-            line = re.sub('togodx.dbcls.jp/human', 'sparql-support.dbcls.jp/dxs_separate_table/build', line)
-            line = re.sub('\?togoKey=', '?dataset=', line)
-            line = re.sub('&keys=', '&annotations=', line)
-            line = re.sub('&values=', '&filters=', line)
-            line = re.sub('%7B%22attributeId%22%3A', '%7B%22attribute%22%3A', line)
-            line = re.sub('%2C%22ids%22%3A', '%2C%22nodes%22%3A', line)
-            line = re.sub('%7B%22categoryId%22', '%7B%22node%22', line)
-            line = re.sub('%22ancestors%22', '%22path%22', line)
-        elif args.dxs:
-            line = re.sub('togodx.dbcls.jp/human', 'sparql-support.dbcls.jp/dxs/build', line)
-            line = re.sub('\?togoKey=', '?dataset=', line)
-            line = re.sub('&keys=', '&annotations=', line)
-            line = re.sub('&values=', '&filters=', line)
-            line = re.sub('%7B%22attributeId%22%3A', '%7B%22attribute%22%3A', line)
-            line = re.sub('%2C%22ids%22%3A', '%2C%22nodes%22%3A', line)
-            line = re.sub('%7B%22categoryId%22', '%7B%22node%22', line)
-            line = re.sub('%22ancestors%22', '%22path%22', line)
-        elif args.dxsk:
-            line = re.sub('togodx.dbcls.jp/human', 'sparql-support.dbcls.jp/dxsk/build', line)
-            line = re.sub('\?togoKey=', '?dataset=', line)
-            line = re.sub('&keys=', '&annotations=', line)
-            line = re.sub('&values=', '&filters=', line)
-            line = re.sub('%7B%22attributeId%22%3A', '%7B%22attribute%22%3A', line)
-            line = re.sub('%2C%22ids%22%3A', '%2C%22nodes%22%3A', line)
-            line = re.sub('%7B%22categoryId%22', '%7B%22node%22', line)
-            line = re.sub('%22ancestors%22', '%22path%22', line)
-        print(line)
-    else:
-        print(line)
+    print(line)
